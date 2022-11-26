6ce5f0e98b5076f22d00555e6fd0313f7752d76e
Jango0712 <gomezmarchanj@gmail.com>
2022-11-26 22:46:09 +0100

    correcion

---
+++ ProyectosJavascript/proyecto1/index.js
@@ -0,0 +1,32 @@
+let suma = (x, y = 0) => {
+    
+    let resultado = x + y;
+
+    console.log(resultado);
+  };
+
+  let resta = (x, y = 0) =>{
+    
+    let resultado = x - y;
+    console.log(resultado);
+  };
+
+  let mult = (x, y = 0) =>{
+  
+    let resultado = x * y;
+    console.log(resultado);
+  };
+
+  let div = (x, y = 0) =>{
+  
+    let resultado = x / y;
+    console.log(resultado);
+  };
+
+  let cuadradoNumero = (x) => {
+    let resultado = x * x;
+    console.log(resultado);
+ }
+  
+
+  module.exports = {suma, resta, mult, div, cuadradoNumero};
